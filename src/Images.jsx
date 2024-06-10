2dab55d99742eb93e5307204365dd3f0ff2056b2
Thomas Reynoso <144083966+Thomas45125@users.noreply.github.com>
2024-06-10 16:32:23 -0300

    Update Images.jsx

--- src/Images.jsx
+++ src/Images.jsx
@@ -5,9 +5,9 @@ export const Images = () => {
     <>
     <div className='imagesleng'>
         <img className='react' src="./assets/html.png" alt="html" />
-        <img className='react' src="src/images/css.png" alt="css" />
-        <img className='react' src="src/images/js.jpg" alt="js" />
-        <img className='react' src="src/images/react.png" alt="react" />
+        <img className='react' src="./assets/css.png" alt="css" />
+        <img className='react' src="./assets/js.jpg" alt="js" />
+        <img className='react' src="./assets/react.png" alt="react" />
     </div>
     <br />
     <br />
